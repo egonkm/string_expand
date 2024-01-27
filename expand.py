6eabbb31ffa72becd0d54776b4ca7c1b60c945e3
egon <egonkirchof@yahoo.com.br>
2024-01-27 18:54:02 +0100

    added functions find_end and split

--- expand.py
+++ expand.py
@@ -10,9 +10,123 @@ OPEN = "({"
 CLOSE = ")}"
 
 
+
+def find_end(string, ptr, close):
+
+  # find the last character of a group
+  # close : list of ending chars
+
+  while close:
+
+    ptr += 1
+    
+    if ptr>=len(s): return -1, None # end of s reached without closing char
+    
+    c = string[ptr]
+
+    if c == close[-1]: # found a matching end group char
+
+      close.pop()
+      continue
+
+    if c in OPEN: # add an open group char
+
+      close.append(CLOSE[OPEN.find(c)])
+      continue
+
+    if c in CLOSE:
+      return -1, ptr  # closing char mismatch
+
+  return ptr+1, None # returns the position of the end of the group
+
+def split(string, sep="|"):
+  # split a group in its elements
+  # sep: separator char
+
+  queue = []
+  ptr = 0
+  start = 0
+  res = []
+
+  while ptr<len(string):
+      
+    c = string[ptr]
+
+    if c in OPEN:
+      queue.append(CLOSE[OPEN.find(c)])
+
+    if c==sep and not queue:
+
+      res.append(string[start:ptr])
+      start = ptr+1
+
+    if c in CLOSE:
+
+      if c==queue[-1]:
+        queue.pop()
+      else:
+        print("Mismatch: ", queue)
+        return None
+
+    ptr += 1
+
+  res.append(string[start:ptr]) # adds the last element of the group
+
+  return res
+
 def expand_it(string, l_expansions, ptr_char):
     
-    return []
+    while ptr_char<len(string):
+
+      char = string[ptr_char]
+
+      if char in OPEN: # group oppening found
+
+        end_car = CLOSE[ OPEN.find(char)]
+        cont, end = find_end(string, 
+                             ptr_char, 
+                             [end_car] ) # get the last char of the group
+
+        if cont==-1: # not found ending char
+
+          if end:
+              
+            print("Error in col ", end+1, 
+                  "looking for ", end_car, "found ", s[end], "instead.")
+            return None
+
+          print("Error: reached end of str without closing char ", end_car,".")
+          
+          return None
+
+        before = string[0:ptr_char]
+        group = string[ptr_char+1:cont-1]
+        after = string[cont:]
+        elements = split(group, "|")
+
+        if char == "{": elements.append("") # expand without anything inside optional group
+ 
+        for posibility in elements:
+
+          if not expand_it(before+posibility+after, 
+                           l_expansions, 
+                           ptr_char): return None
+
+        return True
+
+      if char in CLOSE:
+          
+        print("Closing ",char," without pair, col:", ptr_char+1)
+        return False
+
+      ptr_char += 1
+
+    # while "  " in string: string = string.replace("  ", " ")
+
+    l_expansions.append(string)
+
+    return True
+        
 
 def expand(string):
     
@@ -30,8 +144,8 @@ def expand(string):
 if __name__=="__main__":
     
     s = """
-    Oh, {thank you so much!|God bless you!|I can't believe it!}. 
-This is {way} better than I (expected|needed|asked for).
+    Oh{, thank you so much|, God bless you|, I can't believe it}! 
+This is{ way} better than I (expected|needed|asked for).
     """
     s = s.replace("\n", "").strip()
     
